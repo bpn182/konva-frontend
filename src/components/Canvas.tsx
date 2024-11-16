19727792091f6fdf9ac5e3c9a79d8a5f612699ec
Bipin Bhandari <bhandaribpn1996@gmail.com>
2024-11-16 22:40:47 +1100

    Commented methods inside canvas component

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Stage, Layer } from "react-konva";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  addShape,
-  setShapes,
-} from "../redux/shapesSlice";
+import { addShape, setShapes } from "../redux/shapesSlice";
 import { RootState, AppDispatch } from "../redux/store";
 import { listDrawings, loadShapes, saveShapes } from "@/firebase/shapesService";
 import RenderShape from "./RenderShape";
 import ShapeButtons from "./ShapeButtons";
 import SavedDrawings from "./SavedDrawings";
 import CanvasTopControls from "./CanvasTopControls";
+import { ShapeNames } from "@/interfaces";
 
 interface ShapeProperties {
   radius: number;
@@ -19,44 +17,56 @@ interface ShapeProperties {
 }
 
 const Canvas: React.FC = () => {
-  const [drawingShape, setDrawingShape] = useState<
-    "circle" | "rectangle" | "line" | null
-  >(null);
-  const [shapeProperties, setShapeProperties] = useState<ShapeProperties>({
-    radius: 30,
-    width: 60,
-    height: 40,
-  });
   const dispatch = useDispatch<AppDispatch>();
   const shapes = useSelector((state: RootState) => state.shapes.shapes);
   const userId = useSelector((state: RootState) => state.user.userId) as string;
   const [drawingName, setDrawingName] = useState("");
   const [drawings, setDrawings] = useState<string[]>([]);
   const [currentDrawing, setCurrentDrawing] = useState<string | null>(null);
+  const [drawingShape, setDrawingShape] = useState<ShapeNames>(null);
+  const [shapeProperties, setShapeProperties] = useState<ShapeProperties>({
+    radius: 30,
+    width: 60,
+    height: 40,
+  });
 
   useEffect(() => {
     const loadDrawings = async () => {
+      // Fetch the list of drawings from Firestore for the current user
       const drawingsList = await listDrawings(userId);
+      // Update the state with the fetched drawings list
       setDrawings(drawingsList);
     };
+
     loadDrawings();
   }, [dispatch, userId]);
 
+  /**
+   * Handles saving the current shapes to Firestore under a specified drawing name.
+   * If the drawing name already exists, an alert is shown.
+   * Otherwise, the shapes are saved and the drawings list is updated.
+   */
   const handleSave = async () => {
     if (drawingName) {
+      // Fetch the list of existing drawings for the user
       const drawingsList = await listDrawings(userId);
       if (drawingsList.includes(drawingName)) {
+        // Alert if the drawing name already exists
         alert("Drawing name already exists. Please choose a different name.");
       } else {
+        // Save the shapes under the specified drawing name
         await saveShapes(userId, drawingName, shapes);
+        // Update the drawings list and clear the drawing name input
         setDrawings(drawingsList);
         setDrawingName("");
       }
     } else {
+      // Alert if no drawing name is entered
       alert("Please enter a drawing name.");
     }
   };
 
+  // Handles drawing a new shape on the canvas.
   const handleShapeDraw = (e: any) => {
     if (!drawingShape) return;
 
@@ -64,7 +74,7 @@ const Canvas: React.FC = () => {
     const pointerPosition = stage.getPointerPosition();
 
     const newShape = {
-      id: Date.now().toString(), // Generate a unique ID based on the timestamp
+      id: Date.now().toString(),
       x: pointerPosition.x,
       y: pointerPosition.y,
       shapeType: drawingShape,
@@ -72,10 +82,12 @@ const Canvas: React.FC = () => {
       width: shapeProperties.width, // for rectangle
       height: drawingShape === "line" ? 2 : shapeProperties.height, // for line
     };
+
     // Dispatch action to add the shape to Redux store
     dispatch(addShape(newShape));
   };
 
+  // Handles selecting a shape type and setting its properties
   const handleShapeSelect = (
     shapeType: "circle" | "rectangle" | "line" | null
   ) => {
@@ -89,12 +101,16 @@ const Canvas: React.FC = () => {
     }
   };
 
+  // Handles loading a drawing by its name
   const handleLoadDrawing = async (name: string) => {
     const loadedShapes = await loadShapes(userId, name);
+
+    // Update the Redux store with the loaded shapes
     dispatch(setShapes(loadedShapes));
     setCurrentDrawing(name);
   };
 
+  // Handles updating the current drawing with the latest shapes
   const handleUpdate = async () => {
     if (currentDrawing) {
       await saveShapes(userId, currentDrawing, shapes);
---
+++ src/interfaces/index.ts
@@ -0,0 +1,1 @@
+export type ShapeNames = "circle" | "rectangle" | "line" | null;
